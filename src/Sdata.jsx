d7fdf0bc7d56194b8e1c4a4c435d1dd6d43d8fdd
ashishkr-18 <67505020+ashishkr-18@users.noreply.github.com>
2020-10-25 08:53:41 +0530

    my second commit

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -1,35 +1,41 @@
 const Sdata =[
     {
+        id: 1,
         imgscr:"https://wallpapercave.com/wp/wp5770344.jpg",
         title:"A Netflix Original Series",
         sname:"DARK",
         link:"https://www.netflix.com/in/title/80100172",
     },
     {
+        id: 2,
         imgscr:"https://wallpapercave.com/wp/wp1839578.jpg",
         title:"A Netflix Original Series",
         sname:"STRANGER THINGS",
         link:"https://www.netflix.com/in/title/80057281",
     },
     {
+        id: 3,
         imgscr:"https://wallpapercave.com/wp/wp3814512.png",
         title:"A Netflix Original Series",
         sname:"SACRED GAMES",
         link:"https://www.netflix.com/in/title/80115328",
     },
     {
+        id: 4,
         imgscr:"https://wallpapercave.com/wp/wp4547307.png",
         title:"A Netflix Original Series",
         sname:"THE MINDHUNTER",
         link:"https://www.netflix.com/in/title/80114855",
     },
     {
+        id: 5,
         imgscr:"https://wallpapercave.com/wp/wp5125014.jpg",
         title:"A Netflix Original Series",
         sname:"THE WITCHER",
         link:"https://www.netflix.com/in/title/80189685",
     },
     {
+        id: 6,
         imgscr:"https://wallpapercave.com/wp/wp3237903.jpg",
         title:"A Netflix Original Series",
         sname:"THE PUNISHER",
--- src/index.js
+++ src/index.js
@@ -4,24 +4,13 @@ import Card from "./Cards";
 import "./index.css";
 import Sdata from "./Sdata";
 
-// console.log(Sdata[0].sname);
-// function ncard(val){
-//   return(
-//     <Card
-//     imgscr={val.imgscr}
-//     title={val.title}
-//     sname={val.sname}
-//     link={val.link}
-//     />
-//   );
-// }
-
 ReactDOM.render( 
   <>
     <h1 className="heading_style">List of top Netflix series in 2020</h1>
     {Sdata.map((val) => {
       return (
       <Card
+      key={val.id}
       imgscr={val.imgscr}
       title={val.title}
       sname={val.sname}
